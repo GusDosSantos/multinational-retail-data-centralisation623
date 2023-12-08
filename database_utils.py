6a2d24134fb5991211f9fb1a9844d2264e8e0b63
Gus Dos Santos <GusDSFX@hotmail.com>
2023-12-08 21:27:21 +0000

    yaml_read

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,9 @@
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+
 class DatabaseConnector: 
-    def __init__(self):
-        print("Placeholder")
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as yaml_file:
+            data = yaml.safe_load(yaml_file)
+            return data
